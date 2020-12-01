3194b5f1b45e0f0352d1c81e879d5dc2a5831534
Philipp Schmid <schmidphilipp1995@gmail.com>
2020-12-01 21:56:46 +0100

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+
+
+def handler(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+    return response
